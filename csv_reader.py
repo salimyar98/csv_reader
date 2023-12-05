0eaebc660c16e95de52615ba7aa3447c1dd2c98e
Yaroslav Salimyanov <salimyar98@gmail.com>
2023-12-06 01:16:04 +0300

    Рефакторинг кода Убрал повторения и добавил функцию для извлечения аргументов

--- csv_reader.py
+++ csv_reader.py
@@ -8,38 +8,33 @@ with open(data) as f:
     headers = next(lines)
     lines = list(lines)
 
-dic_str = {i[0]: i for i in lines}
+dict_str = {i[0]: i for i in lines}
+
+
+def arguments(tmp, dict_str, headers, num):
+    arg = dict_str.get(re.sub("[^0-9]", "", tmp[num]), [])
+    return int(arg[headers.index(re.sub("[^A-Za-z]", "", tmp[num]))])
+
 
 for (n, i) in enumerate(lines):
     for (b, m) in enumerate(i):
-        match m:
-            case m if m.startswith('=') and '+' in m:
-                tmp = m.split('+')
-                arg_a = dic_str[re.sub("[^0-9]", "", tmp[0])][headers.index(re.sub("[^A-Za-z]", "", tmp[0]))]
-                arg_b = dic_str[re.sub("[^0-9]", "", tmp[1])][headers.index(re.sub("[^A-Za-z]", "", tmp[1]))]
-                result = int(arg_a) + int(arg_b)
-                i[b] = result
-            
-            case m if m.startswith('=') and '-' in m:
-                tmp = m.split('-')
-                arg_a = dic_str[re.sub("[^0-9]", "", tmp[0])][headers.index(re.sub("[^A-Za-z]", "", tmp[0]))]
-                arg_b = dic_str[re.sub("[^0-9]", "", tmp[1])][headers.index(re.sub("[^A-Za-z]", "", tmp[1]))]
-                result = int(arg_a) - int(arg_b)
-                i[b] = result
-            
-            case m if m.startswith('=') and '/' in m:
-                tmp = m.split('/')
-                arg_a = dic_str[re.sub("[^0-9]", "", tmp[0])][headers.index(re.sub("[^A-Za-z]", "", tmp[0]))]
-                arg_b = dic_str[re.sub("[^0-9]", "", tmp[1])][headers.index(re.sub("[^A-Za-z]", "", tmp[1]))]
-                if arg_b != '0':
-                    result = int(arg_a) / int(arg_b)
-                    i[b] = result
-            
-            case m if m.startswith('=') and '*' in m:
-                tmp = m.split('*')
-                arg_a = dic_str[re.sub("[^0-9]", "", tmp[0])][headers.index(re.sub("[^A-Za-z]", "", tmp[0]))]
-                arg_b = dic_str[re.sub("[^0-9]", "", tmp[1])][headers.index(re.sub("[^A-Za-z]", "", tmp[1]))]
-                result = int(arg_a) * int(arg_b)
+        if m.startswith('='):
+            operators = ['+', '-', '/', '*']
+            operator = next((j for j in operators if j in m), None)
+            if operator:
+                tmp = m.split(operator)
+                arg_a = arguments(tmp, dict_str, headers, 0)
+                arg_b = arguments(tmp, dict_str, headers, 1)
+                match operator:
+                    case '+':
+                        result = arg_a + arg_b
+                    case '-':
+                        result = arg_a - arg_b
+                    case '/':
+                        if arg_b != '0':
+                            result = arg_a / arg_b
+                    case '*':
+                        result = arg_a * arg_b
                 i[b] = result
 
 line_print = {str(n): i for n, i in enumerate(lines)}
